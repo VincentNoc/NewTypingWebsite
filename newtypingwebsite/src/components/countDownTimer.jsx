016d9886e31f7ebf27bbdd61859e1c5c377a218f
VincentNoc <kst0629@autuni.ac.nz>
2024-01-22 13:46:44 +1300

    countDownTimer
    countDownTimer component has been created.

---
+++ newtypingwebsite/src/components/countDownTimer.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CountDownTimer = ({ timeLeft }) => {
+  return <h2 className="text-yellow-500">Time: {timeLeft}</h2>;
+};
+
+export default CountDownTimer;
--- newtypingwebsite/src/pages/typingPage.jsx
+++ newtypingwebsite/src/pages/typingPage.jsx
@@ -1,9 +1,44 @@
-import React from "react";
+import { fetchData, seperateQuote } from "../services/api";
+import React, { useState, useEffect } from "react";
+import CountDownTimer from "../components/countDownTimer";
+export default function TypingPage() {
+  const [randomWords, setRandomWords] = useState([]);
+  const [error, setError] = useState(null);
 
-export default function typingPage() {
+  //this fetches the response from the api service
+  const FetchRandomQuoteFromApi = async () => {
+    try {
+      const response = await fetchData();
+      const seperateResponse = seperateQuote(response);
+      setRandomWords(seperateResponse);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  useEffect(() => {
+    FetchRandomQuoteFromApi();
+  }, []);
+
+  const displayQuote = randomWords.map((word, index) => (
+    <span key={index}>
+      <span className="inline-block p-1">{word}</span>
+    </span>
+  ));
+
+  // console.log("hi there");
+  // console.log(FetchRandomQuoteFromApi);
+
   return (
     <div className="bg-gray-800 h-screen">
-      <div className="flex flex-col items-center justify-center bg-gray-800 h-screen w-screen"></div>
+      <div className="flex flex-col items-center justify-center bg-gray-800 h-screen w-screen">
+        <div className="flex flex-col">
+          <CountDownTimer timeLeft={30} />
+        </div>
+        <p className="text-white font-Hedvig text-2xl tracking-wider">
+          {displayQuote}
+        </p>
+      </div>
     </div>
   );
 }
