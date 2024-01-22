f72cc231ffabd325c2cce6e6e4135cac677cf366
VincentNoc <kst0629@autuni.ac.nz>
2024-01-22 16:47:11 +1300

    homeToType

--- newtypingwebsite/src/pages/home.jsx
+++ newtypingwebsite/src/pages/home.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../style.css";
-import HomeToTypeButton from "../components/homeToTypeButton";
+import HomeToTypeButton from "../components/homeToTypeButton.jsx";
 
 export default function Home() {
   return (
